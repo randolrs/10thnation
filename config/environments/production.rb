165f09b203d572e903cffe37089faaf09188fffd
randolrs <r.shane.randolph@gmail.com>
2016-07-28 17:57:50 -0500

    heroku s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://stark-depths-72565.herokuapp.com' }
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
