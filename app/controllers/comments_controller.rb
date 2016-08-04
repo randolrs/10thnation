3a15ec89dd7fa1788a9ace69c5fe84ac5d8250f0
randolrs <r.shane.randolph@gmail.com>
2016-08-04 17:03:41 -0500

    merg

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,10 +30,10 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { redirect_to root_path}
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
